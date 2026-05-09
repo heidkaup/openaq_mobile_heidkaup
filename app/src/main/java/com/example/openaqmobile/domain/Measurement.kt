573918f067e5bd906f25c75e8867816c943f833e
Heidi Kauppia <heidkaup@edu.lapinamk.fi>
2026-05-09 16:26:58 +0200

    Added Measurement.kt

--- app/src/main/java/com/example/openaqmobile/domain/Measurement.kt
+++ app/src/main/java/com/example/openaqmobile/domain/Measurement.kt
@@ -1,3 +1,9 @@
 package com.example.openaqmobile.domain
 
-data class Measurement()
+data class Measurement(
+    val id: Int,
+    val location: String,
+    val parameter: String,
+    val value: Double,
+    val unit: String
+)
