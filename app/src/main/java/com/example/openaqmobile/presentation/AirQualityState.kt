70dfe0d0fa71fab17a07a6ebc713be78a6d6616d
Heidi Kauppia <heidkaup@edu.lapinamk.fi>
2026-05-09 16:43:40 +0200

    Added AirQualityViewModel.kt

--- app/src/main/java/com/example/openaqmobile/presentation/AirQualityState.kt
+++ app/src/main/java/com/example/openaqmobile/presentation/AirQualityState.kt
@@ -5,5 +5,5 @@ import com.example.openaqmobile.domain.Measurement
 data class AirQualityState(
     val loading: Boolean = false,
     val items: List<Measurement> = emptyList(),
-    val error: String? = null
+    val err: String? = null
 )
--- app/src/main/java/com/example/openaqmobile/presentation/AirQualityViewModel.kt
+++ app/src/main/java/com/example/openaqmobile/presentation/AirQualityViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.openaqmobile.presentation
 
-class AirQualityViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.openaqmobile.domain.OpenAQApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class AirQualityViewModel(private val api: OpenAQApi) : ViewModel() {
+    private val _state = MutableStateFlow(AirQualityState())
+    val state = _state.asStateFlow()
+
+    fun loadData() {
+        _state.update { it.copy(loading = true) }
+
+        viewModelScope.launch {
+            try {
+                val data = api.getMeasurements()
+                _state.update { it.copy(items = data, loading = false) }
+            } catch (e: Exception) {
+                _state.update { it.copy(loading = false, err = e.message) }
+            }
+        }
+    }
 }
