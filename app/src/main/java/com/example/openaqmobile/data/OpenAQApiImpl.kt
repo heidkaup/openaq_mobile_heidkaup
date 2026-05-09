1138491da0b4e4de5408e679f9c9b4140c648307
Heidi Kauppia <heidkaup@edu.lapinamk.fi>
2026-05-09 16:31:43 +0200

    Added OpenAQApiImpl.kt

--- app/src/main/java/com/example/openaqmobile/data/OpenAQApiImpl.kt
+++ app/src/main/java/com/example/openaqmobile/data/OpenAQApiImpl.kt
@@ -1,3 +1,14 @@
 package com.example.openaqmobile.data
 
-class OpenAQApiImpl 
+import com.example.openaqmobile.domain.Measurement
+import com.example.openaqmobile.domain.OpenAQApi
+
+class OpenAQApiImpl : OpenAQApi {
+    override suspend fun getMeasurements(): List<Measurement> {
+        // palautetaan testausta varten
+        return listOf(
+            Measurement(1, "Helsinki", "pm10", 22.4, "µg/m³"),
+            Measurement(2, "Helsinki", "no2", 11.6, "µg/m³")
+        )
+    }
+}
