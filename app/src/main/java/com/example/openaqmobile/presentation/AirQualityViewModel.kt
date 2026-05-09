4324acdf21dba7a032ec81a2e3f5632395cd9202
Heidi Kauppia <heidkaup@edu.lapinamk.fi>
2026-05-09 18:12:46 +0200

    Setup viewmodel for API

--- app/src/main/java/com/example/openaqmobile/presentation/AirQualityViewModel.kt
+++ app/src/main/java/com/example/openaqmobile/presentation/AirQualityViewModel.kt
@@ -1,26 +1,20 @@
 package com.example.openaqmobile.presentation
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.openaqmobile.OpenAQApplication
 import com.example.openaqmobile.domain.OpenAQApi
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 
+// rajapinta
 class AirQualityViewModel(private val api: OpenAQApi) : ViewModel() {
-    private val _state = MutableStateFlow(AirQualityState())
-    val state = _state.asStateFlow()
-
-    fun loadData() {
-        _state.update { it.copy(loading = true) }
-
-        viewModelScope.launch {
-            try {
-                val data = api.getMeasurements()
-                _state.update { it.copy(items = data, loading = false) }
-            } catch (e: Exception) {
-                _state.update { it.copy(loading = false, err = e.message) }
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                // haetaan API OpenAQApplicationista
+                val application = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as OpenAQApplication)
+                AirQualityViewModel(api = application.api)
             }
         }
     }
