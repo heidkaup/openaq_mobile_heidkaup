3cf2a7940569f0f315c345e0657d95a34cee290a
Heidi Kauppia <heidkaup@edu.lapinamk.fi>
2026-05-11 19:56:34 +0200

    delete OpenAQApiImpl.kt and fix measurments

--- app/src/main/java/com/example/openaqmobile/domain/Measurement.kt
+++ app/src/main/java/com/example/openaqmobile/domain/Measurement.kt
@@ -1,9 +1,6 @@
 package com.example.openaqmobile.domain
 
 data class Measurement(
-    val id: Int,
-    val location: String,
-    val parameter: String,
-    val value: Double,
-    val unit: String
+    val measured_at: String?,
+    val value: Double?
 )
